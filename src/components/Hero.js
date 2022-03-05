c0f3d70ed43096ed376782ea15fd77bc12129610
hafizbali315 <hafizbali315@gmail.com>
2022-03-05 05:24:11 +0500

    Hero Section

---
+++ src/components/Hero.js
@@ -0,0 +1,35 @@
+import ShapeMonsters from '../assets/ShapeMonsters.png'
+import YellowMonster from '../assets/YellowMonster.png'
+import PinkMonster from '../assets/PinkMonster.png'
+import GoldMonster from '../assets/GoldMonster.png'
+import MintingArrows from '../assets/MintingArrows.png'
+
+const Hero = () => {
+	return (
+		<div className="hero_section">
+			<img className="shape_monster" src={ShapeMonsters} alt="ShapeMonsters" />
+
+			<p>Shape Monster are one of the first NFT collections on the Moonbeam Network consisting of 3,333 algorithmically generated NFTs. </p>
+
+			<div className="monsters_row">
+				<img src={YellowMonster} alt="YellowMonster" />
+				<img src={PinkMonster} alt="PinkMonster" />
+				<img src={GoldMonster} alt="GoldMonster" />
+			</div>
+
+			<h2>PUBLIC SALE: TO BE ANNOUNCED</h2>
+
+			<div className="minted">
+				<h1>0/3333</h1>
+				<span>minted</span>
+			</div>
+
+			<div className="mint_button_container">
+				<img src={MintingArrows} alt="MintingArrows" />
+				<button>MINT</button>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
